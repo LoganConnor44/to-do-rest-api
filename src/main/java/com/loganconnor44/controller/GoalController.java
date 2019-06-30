cf9ec6fbd6039dcedf8e71696d73a8a02b10de28
Logan Connor <loganconnor44@gmail>
2019-06-30 19:04:38 -0400

    added docblocks to methods

--- src/main/java/com/loganconnor44/controller/GoalController.java
+++ src/main/java/com/loganconnor44/controller/GoalController.java
@@ -16,6 +16,18 @@ public class GoalController {
     @Autowired
     private IGoalService goalService;
 
+    /**
+     * Creates and saves a goal via http post.
+     * <p>
+     * Checks if goal name has already been created by user.
+     * If it has, do not recreate goal.
+     * After goal is created, return back the generated id in the header.
+     * Return an http 201 created status to the user.
+     *
+     * @param goal    Json sent to the application to define the desired goal.
+     * @param builder
+     * @return ResponseEntity<void>
+     */
     @PostMapping()
     public ResponseEntity<Void> addGoal(@RequestBody Goal goal, UriComponentsBuilder builder) {
         boolean flag = goalService.addGoal(goal);
@@ -27,18 +39,47 @@ public class GoalController {
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 
+    /**
+     * Retrieves a goal from the database.
+     * <p>
+     * The client passes the goal id of the desired goal.
+     * Return to the user a 200 http status okay.
+     *
+     * @param goalId The desired goal's id.
+     * @return ResponseEntity<Goal>
+     */
     @GetMapping("/{goalId}")
     public ResponseEntity<Goal> getGoalById(@PathVariable("goalId") Integer goalId) {
         Goal goal = goalService.getGoalById(goalId);
         return new ResponseEntity<Goal>(goal, HttpStatus.OK);
     }
 
+    /**
+     * Updates an existing goal's status as completed.
+     * <p>
+     * The client passes the goal id of the desired goal.
+     * The goal is updated via a defined status enum.
+     * Return to the user a 200 http status okay.
+     *
+     * @param goalId The desired goal's id.
+     * @return ResponseEntity<Goal>
+     */
     @PutMapping("/{goalId}")
     public ResponseEntity<Goal> markGoalAsComplete(@PathVariable("goalId") Integer goalId) {
         goalService.markGoalAsComplete(goalId);
         return new ResponseEntity<Goal>(HttpStatus.OK);
     }
 
+    /**
+     * Deletes an existing goal from the database.
+     * <p>
+     * The client passes the goal id of the desired goal.
+     * The goal is removed from the database.
+     * Return to the user a 204 http no content status.
+     *
+     * @param goalId The desired goal's id.
+     * @return ResponseEntity<Void>
+     */
     @DeleteMapping("/{goalId}")
     public ResponseEntity<Void> deleteGoal(@PathVariable("goalId") Integer goalId) {
         goalService.deleteGoal(goalId);
